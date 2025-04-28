55006523e712d4d76d6b8fc532b8262b53f8465a
mustafinmp <mustafinmp@yandex.ru>
2025-04-28 17:24:51 +0500

    Сделана задача FunctionalInterfaces

--- src/test/java/edu/phystech/hw2/FunctionalInterfacesTest.java
+++ src/test/java/edu/phystech/hw2/FunctionalInterfacesTest.java
@@ -15,7 +15,7 @@ import org.junit.jupiter.api.Assertions;
 class ToUpperCaseOperator implements UnaryOperator<String> {
     @Override
     public String apply(String s) {
-        return s;
+        return s.toUpperCase();
     }
 }
 
@@ -24,7 +24,7 @@ class AbsMaxOperator implements BinaryOperator<Integer> {
 
     @Override
     public Integer apply(Integer integer, Integer integer2) {
-        return 0;
+        return Integer.max(Math.abs(integer), Math.abs(integer2));
     }
 }
 
@@ -32,7 +32,7 @@ class StringLengthMoreThan5 implements Predicate<String> {
 
     @Override
     public boolean test(String s) {
-        return true;
+        return s.length() > 5;
     }
 }
 
@@ -42,18 +42,23 @@ class IsNumberASquareOfAnotherNumber implements Predicate<Integer> {
 
     @Override
     public boolean test(Integer integer) {
-        return true;
+        return integer == Math.pow(Math.sqrt(integer), 2);
     }
 }
 
 // Возвращает четные числа, начиная с from включительно, если в from нечетное число, то начиная с первого четного с from
 class EvenNumberSupplier implements Supplier<Integer> {
+    private int currentNum;
 
-    public EvenNumberSupplier(int from) {}
+    public EvenNumberSupplier(int from) {
+        this.currentNum = from % 2 == 0 ? from : from + 1;
+    }
 
     @Override
     public Integer get() {
-        return 0;
+        int result = currentNum;
+        currentNum += 2;
+        return result;
     }
 }
 
